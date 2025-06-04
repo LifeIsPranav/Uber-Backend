dfc0544629757c42309ee255ce62c75f5c72e315
Pranav <lifeispranav@gmail.com>
2025-06-05 03:38:49 +0530

    Captain routers added for getProfile and logout

--- src/routes/captain.routes.js
+++ src/routes/captain.routes.js
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 import { loginValidator, registerValidation } from '../validators/captain.validator.js';
-import { loginCaptain, registerCaptain } from '../controllers/captain.controller.js';
+import { getCaptainProfile, loginCaptain, logoutCaptain, registerCaptain } from '../controllers/captain.controller.js';
 import { authCaptain } from '../middleware/auth.middleware.js';
 
 // import { authUser } from '../middleware/auth.middleware.js';
@@ -11,8 +11,8 @@ const captainRouter = express.Router();
 
 captainRouter.post('/register', registerValidation, registerCaptain)
 captainRouter.post('/login', loginValidator, loginCaptain)
-// captainRouter.get('/profile', authCaptain)
-// captainRouter.get('/logout', authCaptain)
+captainRouter.get('/profile', authCaptain, getCaptainProfile)
+captainRouter.get('/logout', authCaptain, logoutCaptain)
 
 
 export default captainRouter;
