acfc7906c8d1e36dd84f30899ac9f2dad52b80ba
Pranav <lifeispranav@gmail.com>
2025-06-05 03:34:57 +0530

    Feat: added middleware for captain authorization

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 import userModel from "../models/user.models.js";
+import captainModel from "../models/captain.model.js";
 
 export const authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
@@ -13,6 +14,26 @@ export const authUser = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await userModel.findById(decoded.id)
     req.user = user;
+    next();
+    
+  } catch (error) {
+    console.error("Authentication error:", error);
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
+
+
+export const authCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const captain = await captainModel.findById(decoded.id)
+    req.captain = captain;
     next();
     
   } catch (error) {
